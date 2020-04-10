708a8eca31eed81ee747e9919e74a66423e153df
Vitor E <vitor.rodrigues@al.resilia.com.br>
2020-04-10 20:00:14 -0300

    criando background

---
+++ main.js
@@ -0,0 +1,12 @@
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+let box = 32;
+
+function criarBG() {
+    context.fillStyle = "Lightgreen";
+    context.fillRect(0, 0, 16 * box, 16 * box);
+
+
+}
+
+criarBG();
