270b1b1c7924912b9870aa86b10ec9cbd22925c3
Vitor E <vitor.rodrigues@al.resilia.com.br>
2020-04-10 20:12:24 -0300

    cobrinha criada

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 function criarBG() {
     context.fillStyle = "Lightgreen";
@@ -9,4 +14,13 @@ function criarBG() {
 
 }
 
+function criarCobrinha() {
+    for (i = 0; i < snake.length; i++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+
+    }
+}
+
 criarBG();
+criarCobrinha();
