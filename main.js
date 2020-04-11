5bd10d890c4fb2e5cb40b4067c233ec670f808a3
Vitor E <vitor.rodrigues@al.resilia.com.br>
2020-04-10 21:20:13 -0300

    alvo da cobrinha criado

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ snake[0] = {
 }
 
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box,
+}
 
 function criarBG() {
     context.fillStyle = "Lightgreen";
@@ -22,6 +26,12 @@ function criarCobrinha() {
         context.fillRect(snake[i].x, snake[i].y, box, box);
 
     }
+}
+
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+
 }
 
 document.addEventListener('keydown', update);
@@ -43,6 +53,7 @@ function iniciarJogo() {
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
